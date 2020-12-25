cf777eb4c15d3a83a8a5ec55e21405ad5ff344bf
qq1164110397 <1164110397@qq.com>
2020-12-25 15:28:56 +0800

    提交2

---
+++ app/src/main/java/com/example/p7xiangmu/ShouActicty.java
@@ -0,0 +1,14 @@
+package com.example.p7xiangmu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShouActicty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shou_acticty);
+    }
+}
--- app/src/main/java/com/example/p7xiangmu/fragment/HomeFragment.java
+++ app/src/main/java/com/example/p7xiangmu/fragment/HomeFragment.java
@@ -1,22 +1,53 @@
 package com.example.p7xiangmu.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.p7xiangmu.R;
+import com.example.p7xiangmu.ShouActicty;
+import com.example.p7xiangmu.beans.HomeBean;
+import com.youth.banner.Banner;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView rlv;
+    private Banner banner;
+    private ImageView img;
+    private ArrayList<HomeBean.DataBean.BannerBean> bannerlist;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.home_fragment, null);
-
+        rlv = view.findViewById(R.id.rlv);
+        banner = view.findViewById(R.id.banner);
+        img = view.findViewById(R.id.img);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ShouActicty.class));
+            }
+        });
+
+        bannerlist = new ArrayList<>();
+        new ArrayList<>();
     }
 }
